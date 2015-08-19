6cf46a0fbdf822ba496bfe40c22645bee4ae330a
Sébastien HOUZÉ <s@verylastroom.com>
2015-08-19 15:25:02 +0200

    Throw decode exception when Google Http Response is empty (json_decode('') return null)

--- src/GoogleGeocodeClient.php
+++ src/GoogleGeocodeClient.php
@@ -50,6 +50,12 @@ class GoogleGeocodeClient
             }
 
             $content = (string) $response->getBody();
+            if (empty($content)) {
+                throw new Exception\GoogleGeocodeResponseDecodeException(
+                    'Response is empty, expecting valid json.'
+                );
+            }
+
             $json = json_decode($content, true);
 
             if (null === $json) {
